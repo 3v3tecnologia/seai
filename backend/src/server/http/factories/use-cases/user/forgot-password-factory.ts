34f0f1f39d3daf394f50e44f0f8b0b2c400d4195
Spinnafre <davispenha@gmail.com>
2023-06-23 11:05:57 -0300

    fix: forgot password factory

--- backend/src/server/http/factories/use-cases/user/forgot-password-factory.ts
+++ backend/src/server/http/factories/use-cases/user/forgot-password-factory.ts
@@ -1,7 +1,8 @@
 import { ForgotPassword } from "../../../../../domain/use-cases/user/forgot-password/forgot-password";
 import { AccountRepository } from "../../../../../infra/database/postgres/repositories/account-repository";
+import { makeSendEmailToUser } from "./send-email-to-user-factory";
 
 export const makeForgotPasswordUser = (): ForgotPassword => {
   const accountRepository = new AccountRepository();
-  return new CreateUser(userRepository);
+  return new ForgotPassword(accountRepository, makeSendEmailToUser());
 };
