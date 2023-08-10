2fe1e7c9a576f5d89f59c314907d66a10a5451a0
Spinnafre <davispenha@gmail.com>
2023-08-10 15:29:52 -0300

    fix: add api version in docs

--- db/seeds/government.sql
+++ db/seeds/government.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO public."User"
 ("Name", "Login", "Password", "Type", "CreatedAt", "UpdatedAt", "Email")
-VALUES('admin', 'admin', '$2b$10$.Z6QCr5rODUW5Bz1zUoV9ehzQhbeQ5iWIlw9z.yvEwB44vsIalVa.', 'admin'::public.user_types, '2023-06-28 11:23:10.437', '2023-06-28 11:23:10.437', 'admin@gmail.com');
+VALUES('admin', 'admin', '$2b$10$TI5C2cUFWzeXFabq0IxFaOW4vmx49njQYWl581yic1GRzgPWwnXdy', 'admin'::public.user_types, '2023-06-28 11:23:10.437', '2023-06-28 11:23:10.437', 'admin@gmail.com');
 
 INSERT INTO public."Module"
 ("Name")
