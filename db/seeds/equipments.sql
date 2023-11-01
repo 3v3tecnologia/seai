57f91fd3842d0c20beaa5dcdda4b97bcd237e4d7
LucasAraripe <pedro.lucx@gmail.com>
2023-11-01 14:41:21 -0300

    fix : fixing equipments seed

--- db/seeds/equipments.sql
+++ db/seeds/equipments.sql
@@ -3,13 +3,13 @@
 INSERT INTO "EquipmentType" ("Name")
 VALUES ('station'), ('pluviometer');
 
+INSERT INTO "MetereologicalOrgan"
+("Name", "Host", "User", "Password")
+VALUES('INMET', NULL, NULL, NULL);
+
+INSERT INTO "MetereologicalOrgan"
+("Name", "Host", "User", "Password")
+VALUES('FUNCEME', 'ftp.funceme.br', 'pcds.funceme', 'kDnQgk1h');
+
 INSERT INTO "MetereologicalEquipment" ("IdEquipmentExternal", "Name", "FK_Organ", "FK_Type", "Altitude")
 VALUES ('32321', 'Fortaleza - Itaperi', 2, 1,30.4 );
-
-INSERT INTO "MetereologicalOrgan"
-("IdOrgan", "Name", "Host", "User", "Password")
-VALUES(1, 'INMET', NULL, NULL, NULL);
-
-INSERT INTO "MetereologicalOrgan"
-("IdOrgan", "Name", "Host", "User", "Password")
-VALUES(2, 'FUNCEME', 'ftp.funceme.br', 'pcds.funceme', 'kDnQgk1h');
