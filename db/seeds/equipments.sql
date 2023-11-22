50c4cca375ca2afa5f2482287e9c3999d5674fbd
PedroLucas <pedro.lucx@gmail.com>
2023-11-22 09:00:58 -0300

    removing duplicate key seed

--- db/seeds/equipments.sql
+++ db/seeds/equipments.sql
@@ -11,9 +11,6 @@ INSERT INTO "MetereologicalOrgan"
 ("Name", "Host", "User", "Password")
 VALUES('FUNCEME', 'ftp.funceme.br', 'pcds.funceme', 'kDnQgk1h');
 
-INSERT INTO "MetereologicalEquipment" ("IdEquipmentExternal", "Name", "FK_Organ", "FK_Type", "Altitude")
-VALUES ('32321', 'Fortaleza - Itaperi', 2, 1,30.4 );
-
 INSERT INTO "MetereologicalEquipment"
 ("IdEquipmentExternal", "Name", "Altitude", "FK_Organ", "FK_Type")
 VALUES('32321', 'Fortaleza - Itaperi', 30.4, 2, 1);
