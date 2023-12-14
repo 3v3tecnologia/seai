9efd3b813e6e377ad74eec47a48bbeb68d075452
Spinnafre <davispenha@gmail.com>
2023-12-14 16:29:01 +0000

    feat(Api): serve static files

--- backend/src/server/http/middlewares.ts
+++ backend/src/server/http/middlewares.ts
@@ -2,10 +2,11 @@ import { Express } from "express";
 
 import { bodyParser } from "./http-middlewares/body-parser";
 import { contentType } from "./http-middlewares/content-type";
-import { cors } from "./http-middlewares/cors";
-
+// import { cors } from "./http-middlewares/cors";
+import cors from "cors";
 export function setMiddleware(app: Express): void {
   app.use(bodyParser);
   app.use(contentType);
-  app.use(cors);
+  // app.use(cors);
+  app.use(cors());
 }
