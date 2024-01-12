2cb848a2b5d8af32e07647845a48aaa9c5e79e3a
Lucas Araripe <pedro.lucx@gmail.com>
2024-01-12 10:40:16 -0300

    removing useless warning of no-unused-vars

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     fix: true,
   },
   rules: {
+    "@typescript-eslint/no-unused-vars": "off",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "prettier/prettier": ["error", { endOfLine: "auto" }],
