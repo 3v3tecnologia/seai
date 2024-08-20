f2a1029d767c29e5447b1fbd0890535ab5f0a54d
Spinnafre <davispenha@gmail.com>
2024-08-20 09:43:12 -0300

    fix: user name max and min character validation

--- src/domain/User/lib/model/name.ts
+++ src/domain/User/lib/model/name.ts
@@ -1,7 +1,7 @@
 import { Either, left, right } from "../../../../shared/Either";
 
 export class UserName {
-  public static maxLength: number = 20;
+  public static maxLength: number = 50;
   public static minLength: number = 2;
 
   private name: string;
@@ -17,6 +17,20 @@ export class UserName {
   static create(name: string | null): Either<Error, UserName> {
     if (!name || name.length === 0 || name.length > UserName.maxLength) {
       return left(new Error("Nome não deve ser vazio ou nulo"));
+    }
+    if (name.length > UserName.maxLength) {
+      return left(
+        new Error(
+          `Nome não deve ser maior do que ${UserName.maxLength} caracteres.`
+        )
+      );
+    }
+    if (name.length < UserName.minLength) {
+      return left(
+        new Error(
+          `Nome não deve ser menor do que ${UserName.maxLength} caracteres.`
+        )
+      );
     }
     return right(new UserName(name));
   }
