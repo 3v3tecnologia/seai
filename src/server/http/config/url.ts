5532b1027a7971b1d89a27aca57d25bd3395764e
Spinnafre <davispenha@gmail.com>
2024-08-29 11:29:59 -0300

    fix(user irrigant): get irrigant url redirection link

--- src/server/http/config/url.ts
+++ src/server/http/config/url.ts
@@ -1,4 +1,4 @@
 export const PUBLIC_ASSETS_BASE_URL =
   process.env.PUBLIC_ASSETS_BASE_URL || "http://localhost:8080/static/#";
 
-export const USER_IRRIGANT_PUBLIC_URL = process.env.PUBLIC_ASSETS_BASE_URL || "http://localhost:3333/"
+export const USER_IRRIGANT_PUBLIC_URL = process.env.IRRIGANT_PUBLIC_ASSETS_BASE_URL || "http://localhost:3333/"
