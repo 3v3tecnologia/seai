8b5d11c4aece0d8e08adb5a3146a52fda827af69
Spinnafre <davispenha@gmail.com>
2024-09-05 16:19:08 -0300

    chore: configure helmet to allow swagger

--- src/server/http/app.ts
+++ src/server/http/app.ts
@@ -13,19 +13,19 @@ import { MAX_PAYLOAD_SIZE } from "./config/payload";
 export const setupApp = async (): Promise<Express> => {
   const app = express();
 
-  // app.use(
-  //   helmet.contentSecurityPolicy({
-  //     directives: {
-  //       defaultSrc: ["'self'"],
-  //       fontSrc: ["'self'"],
-  //       imgSrc: ["'self'"],
-  //       scriptSrc: ["'self'"],
-  //       styleSrc: ["'self'"],
-  //       frameSrc: ["'self'"],
-  //     },
-  //     reportOnly: true, // Set to 'true' to enable report-only mode
-  //   })
-  // );
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        fontSrc: ["'self'"],
+        imgSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com'],
+        styleSrc: ["'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com'],
+        frameSrc: ["'self'"],
+      },
+      reportOnly: true, // Set to 'true' to enable report-only mode
+    })
+  );
   app.use(morgan("tiny"));
 
   app.use(express.json({ limit: `${MAX_PAYLOAD_SIZE}mb` }));
