5ca58f5ab5824973d1edb0163eb4091a896b4ffb
Spinnafre <davispenha@gmail.com>
2024-09-16 15:56:26 -0300

    refactor(FAQ): remove useless files

--- src/domain/FAQ/services/protocols/delete-faq-category.ts
+++
@@ -1,9 +0,0 @@
-import { UserCommandOperationProps } from "../../../Logs/protocols/logger";
-import { Either } from "../../../../shared/Either";
-
-export interface DeleteFaqCategoryProtocol {
-  execute(
-    id_category: number,
-    operation: UserCommandOperationProps
-  ): Promise<Either<Error, any>>;
-}
