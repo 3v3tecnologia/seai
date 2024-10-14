88ab5ded5a1d7822364bd4f7e554fe2a28961cbf
Spinnafre <davispenha@gmail.com>
2024-10-14 12:41:01 -0300

    remove public assets

--- src/server/http/app.ts
+++ src/server/http/app.ts
@@ -16,17 +16,6 @@ export const setupApp = async (): Promise<Express> => {
   app.use(express.json({ limit: `2mb` }));
   app.use(express.urlencoded({ extended: true }));
 
-  app.use(
-    "/static",
-    express.static(resolve(__dirname, "..", "..", "..", "public"))
-  );
-
-  app.get("/", (request, response) => {
-    return response.sendFile(
-      resolve(__dirname, "..", "..", "..", "public", "index.html")
-    );
-  });
-
   setApiDocs(app);
   setMiddleware(app);
   setRoutes(app);
