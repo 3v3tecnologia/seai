1a13e63bb91b9f6a351d5882a75840af940a7c86
Spinnafre <davispenha@gmail.com>
2024-10-18 17:40:50 -0300

    fix: remove unnecessary dependencies on irrigation services

--- src/domain/User/services/factories/irrigation-settings.ts
+++ src/domain/User/services/factories/irrigation-settings.ts
@@ -1,12 +1,8 @@
-import { newsletterService } from "../../../Newsletter/services";
-import { IrrigationUserRepository } from "../../infra/repositories/irrigation-user.repository";
 import { IrrigantPreferencesRepository } from "../../infra/repositories/irrigation-settings.repository";
 import { UserSettingsServices } from "../irrigant-settings.service";
 
 
 export const irrigantSettingsService = new UserSettingsServices(
-  new IrrigantPreferencesRepository(),
-  new IrrigationUserRepository(),
-  newsletterService
+  new IrrigantPreferencesRepository()
 );
 
